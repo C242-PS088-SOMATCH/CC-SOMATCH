2a59ad935e5fdcd65f426304c73e3ffa8595b034
Sinichi21 <60685687+Sinichi21@users.noreply.github.com>
2024-12-09 17:54:33 +0800

    fix: bucket

--- src/config/googleCloud.js
+++ src/config/googleCloud.js
@@ -1,5 +1,6 @@
 const { Storage } = require("@google-cloud/storage");
 const storage = new Storage({
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
   projectId: process.env.GCLOUD_PROJECT_ID,
 });
 const bucketName = process.env.GCLOUD_BUCKET_NAME;
