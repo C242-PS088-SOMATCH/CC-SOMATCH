5815374dd5b27a6a0f418782d4f5feab5497851c
Sinichi21 <60685687+Sinichi21@users.noreply.github.com>
2024-12-09 16:37:46 +0800

    fix middleware

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({
     status: "SoMatch API is active",
   });
