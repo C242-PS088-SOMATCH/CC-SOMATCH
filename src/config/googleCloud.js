9768b7e81e519098cf6707499b304d7954c40c39
Gemuh Raharja <60685687+Sinichi21@users.noreply.github.com>
2024-12-09 19:25:22 +0800

    Update googleCloud.js

--- src/config/googleCloud.js
+++ src/config/googleCloud.js
@@ -1,19 +1,7 @@
 const { Storage } = require("@google-cloud/storage");
-const path = require("path");
+const storage = new Storage();
+const bucketName = process.env.GCLOUD_BUCKET_NAME;
 
-// Inisialisasi Google Cloud Storage dan kembalikan bucket
-async function initializeStorage() {
-  try {
-    const storage = new Storage(); // Google Cloud SDK akan menggunakan kredensial default di Cloud Run
-    const bucketName = process.env.GCLOUD_BUCKET_NAME;
-    const bucket = storage.bucket(bucketName);
+const bucket = storage.bucket(bucketName);
 
-    return bucket;
-  } catch (error) {
-    console.error("Error initializing Google Cloud Storage:", error);
-    throw error;
-  }
-}
-
-// Ekspor fungsi inisialisasi bucket untuk digunakan di controller lain
-module.exports = initializeStorage;
+module.exports = bucket;
