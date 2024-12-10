591d7e280f52e6945b14500e127265f26c285a19
Gemuh Raharja <60685687+Sinichi21@users.noreply.github.com>
2024-12-10 23:27:56 +0800

    Revert "fix: callback uri"

--- src/config/auth.js
+++ src/config/auth.js
@@ -17,7 +17,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URI,
+      callbackURL: "/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
