1656b9a8f476e61399aeb8036a60c76656b205d2
Sinichi21 <60685687+Sinichi21@users.noreply.github.com>
2024-12-10 22:53:54 +0800

    fix: googleOauth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,16 +2,21 @@ const jwt = require("jsonwebtoken");
 const secretKey = process.env.JWT_SECRET; // Gantilah dengan secret key Anda
 
 function verifyToken(req, res, next) {
-  const token = req.header("Authorization")?.split(" ")[1];
+  // Ambil token dari header Authorization
+  const authHeader = req.header("Authorization");
+  const token = authHeader && authHeader.split(" ")[1];
+
   if (!token) {
     return res.status(401).json({ message: "Access denied, token is missing" });
   }
 
   try {
+    // Verifikasi token
     const decoded = jwt.verify(token, secretKey);
     req.user = decoded; // Menyimpan data user yang didekodekan dalam request
     next();
   } catch (error) {
+    console.error("Token verification failed:", error.message);
     return res.status(401).json({ message: "Invalid token" });
   }
 }
