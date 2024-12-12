4ddaa653beb995cce290971e04db3615c099c6c5
Sinichi21 <60685687+Sinichi21@users.noreply.github.com>
2024-12-12 12:58:25 +0800

    new: prediction model

--- src/config/tensorFlow.js
+++ src/config/tensorFlow.js
@@ -1,4 +1,5 @@
 const tf = require("@tensorflow/tfjs-node");
+const fs = require("fs");
 
 // Path ke model untuk fitur 1
 const predictionModelPath = "/usr/src/app/src/predicModel/model.json";
@@ -9,6 +10,9 @@ const recommendationModelPath = "/usr/src/app/src/recomendationModel/model.json"
 // Memuat model untuk fitur 1
 const loadPredictionModel = async () => {
   const model = await tf.loadGraphModel(`file://${predictionModelPath}`);
+  if (!fs.existsSync(predictionModelPath)) {
+    console.log("Model file not found:", predictionModelPath);
+  }
   console.log("Prediction model loaded");
   return model;
 };
