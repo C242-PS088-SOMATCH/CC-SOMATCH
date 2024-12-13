4286f39c8f7be7cb48d6c5c991b6a0eefc63bd4a
Sinichi21 <60685687+Sinichi21@users.noreply.github.com>
2024-12-13 10:36:37 +0800

    getRand

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const passport = require("./config/auth");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const imageRoute = require("./routes/imageRoutes");
-const predictionRoute = require("./routes/predictionRoutes");
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,7 +13,6 @@ app.use(passport.initialize());
 app.use("/api/users", userRoutes);
 app.use("/auth", authRoutes);
 app.use("/api/mycatalog", imageRoute);
-app.use("/api", predictionRoute);
 
 const PORT = process.env.APP_PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
